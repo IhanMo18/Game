104f64dcab8395bfd56e8556ea141c6934920608
Ihan <156491555+IhanMo18@users.noreply.github.com>
2024-05-04 18:39:38 -0400

    Add files via upload

---
+++ Gusanito.java
@@ -0,0 +1,12 @@
+package Gusanito;
+
+public class Gusanito {
+   private char[][] posicionA;
+   private char[][] cabezaX;
+   private char[][] cabezaY;
+    private char[][] Mov;
+
+    public int setLocation(char [][] matriz){
+       return matriz.length/2;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,35 @@
+package Gusanito;
+
+public class Main {
+
+    public static void main(String[] args) {
+     Gusanito gusanito=new Gusanito();
+     char[][]matriz=new char[20][20];
+     int large= matriz.length;
+     int location=gusanito.setLocation(matriz);
+
+     //Make Field
+     for (int i=0;i<large;i++){
+         for (int j=0;j<large;j++){
+             if (i==0 || i==(large-1) || j==0 || j==(large-1)){
+                 System.out.print("*");
+             } else if (i==location && j==location) {
+                 System.out.print(">");
+             } else{
+             System.out.print(" ");
+             }
+         }
+         System.out.println();
+     }
+
+
+
+
+
+
+
+
+
+
+    }
+}
